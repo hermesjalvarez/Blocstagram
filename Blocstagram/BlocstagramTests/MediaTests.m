dcb65f6e09fcc9610a8d46259b55b32f7f4ea9b8
Hermes <hermesjalvarez@gmail.com>
2015-11-01 17:08:48 -0500

    chapter 45

---
+++ Blocstagram/BlocstagramTests/MediaTests.m
@@ -0,0 +1,33 @@
+#import <XCTest/XCTest.h>
+#import "Media.h"
+
+
+@interface MediaTests : XCTestCase
+
+@end
+
+@implementation MediaTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testThatInitializationWorks
+{
+    NSDictionary *sourceDictionary = @{@"id": @"8675309",
+                                       @"caption" : @"some caption"};
+    
+    Media *testMedia = [[Media alloc] initWithDictionary:sourceDictionary];
+    
+    XCTAssertEqualObjects(testMedia.idNumber, sourceDictionary[@"id"], @"The ID number should be equal");
+    XCTAssertEqualObjects(testMedia.caption, sourceDictionary[@"caption"], @"The caption should be equal");
+
+}
+
+@end
